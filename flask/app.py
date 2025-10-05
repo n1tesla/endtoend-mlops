733a48ff11f46480cc6fc502fdf0b0753672bcfa
n1tesla <tnakyol@hotmail.com>
2025-10-05 14:45:21 +0300

    dynamic URLs, handling URL parameters and GET,POST

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 '''
 it creates an instance of the Flask class,
@@ -8,12 +8,41 @@ app = Flask(__name__)
 @app.route('/')  #Slash basically means this becomes my home page.
 def welcome():
     return "Welcome to my MLOps application"
+@app.route('/hello',methods=['GET','POST'])
+def hello():
+    #open git bash then paste this curl -X POST http://127.0.0.1:5000/hello (this methods for just POST)
+    #if you want "Hello World without -X, then add GET to methods
+    if request.method == 'GET':
+        return 'You made a GET request to /hello\n'
+    elif request.method == 'POST':
+        return 'You made a POST request to /hello\n'
+    else:
+        return 'you will never see this message\n'
+    return "Hello World"
+@app.route
+
 @app.route('/greet/<name>')
 def greet(name):
-    return f"Hello {name}"
+    #curl -I htpp://127.0.0.1:5000/greet
+    return f"Hello {name}", 202  #Simply give way to status code. default is 200 as well
 @app.route('/index')
 def index():
-    return "<h1>This is the index page</h1>"
+    response = make_response('<h1>Index Page</h1>')
+    response.status_code=202
+    response.headers['content-type'] = 'text/html'
+    return response
+    # return "<h1>This is the index page</h1>"
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f"<h1>{number1}+{number2} = {number1 + number2}</h1>"
+
+@app.route('/handle_url_params')
+def handle_url_params():
+    greeting = request.args.get('greeting')
+    name = request.args.get('name')
+    return f'{greeting}, {name}'  #if one key is missing, it'll give BadReqestKeyError or None
+
 
 
 
