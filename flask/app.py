2a345c9e3d25c44b6ad0e38c872e45c070c92a14
n1tesla <tnakyol@hotmail.com>
2025-10-05 21:23:17 +0300

    Rendering HTML templates, Jinja2 Templating language, Extending templates, Jinja2 filters, Redirect&Dynamic URL Paths

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template,redirect,url_for
 
 '''
 it creates an instance of the Flask class,
@@ -37,14 +37,36 @@ def index():
 def add(number1, number2):
     return f"<h1>{number1}+{number2} = {number1 + number2}</h1>"
 
+@app.route('/test')
+def test():
+    myvalue='Tunahan AKYOL'
+    myresult = 10+20
+    mylist = [10,20,30,40,50]
+    return render_template(template_name_or_list='test.html',myvalue=myvalue,myresult=myresult,mylist=mylist)
+
+
 @app.route('/handle_url_params')
 def handle_url_params():
     greeting = request.args.get('greeting')
     name = request.args.get('name')
     return f'{greeting}, {name}'  #if one key is missing, it'll give BadReqestKeyError or None
 
+@app.route('/other')
+def other():
+    some_text = "Hello Others"
+    return render_template('other.html',some_text=some_text)
 
+@app.template_filter('reverse_string')
+def reverse_string(string):
+    return string[::-1]
 
+@app.template_filter('repeat')
+def repeat(string,times=2):
+    return string*times
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('other'))
 
 if __name__ == '__main__':
     app.run(debug=True)  #you don't need to restart with debug = True
